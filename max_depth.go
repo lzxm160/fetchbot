f520cbe2f7ea4a373960bd0829bc19004b340a67
lzxm160 <lzxm160@sina.com>
2018-05-19 13:00:14 +0800

    add colly

--- max_depth.go
+++ max_depth.go
@@ -15,15 +15,30 @@ func main() {
 	)
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Println(link)
-		// Visit link found on page
-		e.Request.Visit(link)
+	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	// 	link := e.Attr("href")
+	// 	// Print link
+	// 	fmt.Println(link)
+	// 	// Visit link found on page
+	// 	e.Request.Visit(link)
+	// })
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		// t := make([]transcript, 0)
+		// e.ForEach(".topic-media-row", func(_ int, el *colly.HTMLElement) {
+		// 	t = append(t, transcript{
+		// 		Speaker: el.ChildText(".speaker-label"),
+		// 		Text:    el.ChildText(".transcript-text-block"),
+		// 	})
+		// })
+		// jsonData, err := json.MarshalIndent(t, "", "  ")
+		// if err != nil {
+		// 	return
+		// }
+		// ioutil.WriteFile(colly.SanitizeFileName(e.Request.Ctx.Get("date")+"_"+e.Request.Ctx.Get("slug"))+".json", jsonData, 0644)
+		fmt.Println(e)
 	})
-
+	//lianjie body
 	// Start scraping on https://en.wikipedia.org
-	// c.Visit("https://www.ifa.plus/pc/")
-	c.Visit("https://en.wikipedia.org")
+	c.Visit("https://b12.ifa.plus/pc/")
+	// c.Visit("https://en.wikipedia.org")
 }
