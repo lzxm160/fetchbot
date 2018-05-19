748238a546ad0d5c78f6c7706e6f808304248e7a
lzxm160 <lzxm160@sina.com>
2018-05-19 15:11:58 +0800

    add colly

--- max_depth.go
+++ max_depth.go
@@ -6,7 +6,24 @@ import (
 	"time"
 )
 
-func main() {
+var (
+	Host = []string{
+		"https://www.ifa.plus/pc/",
+		"https://b1.ifa.plus/pc/",
+		"https://b2.ifa.plus/pc/",
+		"https://b3.ifa.plus/pc/",
+		"https://b4.ifa.plus/pc/",
+		"https://b5.ifa.plus/pc/",
+		"https://b6.ifa.plus/pc/",
+		"https://b7.ifa.plus/pc/",
+		"https://b8.ifa.plus/pc/",
+		"https://b9.ifa.plus/pc/",
+		"https://b10.ifa.plus/pc/",
+		"https://b11.ifa.plus/pc/",
+		"https://b12.ifa.plus/pc/"}
+)
+
+func start(url string) {
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// MaxDepth is 1, so only the links on the scraped page
@@ -27,6 +44,7 @@ func main() {
 		RandomDelay: 10 * time.Second,
 	})
 	c.OnHTML("body", func(e *colly.HTMLElement) {
+		fmt.Println("e:", e)
 		// t := make([]transcript, 0)
 		// e.ForEach(".topic-media-row", func(_ int, el *colly.HTMLElement) {
 		// 	t = append(t, transcript{
@@ -44,15 +62,27 @@ func main() {
 		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 
 		c.Visit(e.Request.AbsoluteURL(link))
-		fmt.Println("e:", e)
+
 	})
 	//lianjie body
 	// Start scraping on https://en.wikipedia.org
-	c.Visit("https://b12.ifa.plus/pc/")
+	c.Visit(url)
 	// c.Visit("https://en.wikipedia.org")
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting:", r.URL.String())
 
 	})
 	c.Wait()
+}
+func main() {
+	var ticker *time.Ticker = time.NewTicker(time.Duration(10) * time.Second)
+	c := make(chan int, 1)
+	go func() {
+		for _ = range ticker.C {
+			for _, u := range Host {
+				start(u)
+			}
+		}
+	}()
+	<-c
 }
